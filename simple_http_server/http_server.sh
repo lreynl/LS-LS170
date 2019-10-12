eca0eb26af7381202f94aad4bba23c8960617897
lreynl <lr177245@gmail.com>
2019-10-12 16:10:53 -0700

    doing simple http server

--- simple_http_server/http_server.sh
+++ simple_http_server/http_server.sh
@@ -1,13 +1,42 @@
 #!/bin/bash
 
 function server () {
-  while [[ true ]]
+  while true
   do
-    read message
-    echo "You said: $message"
+    message_arr=()
+    check=true
+    while $check
+    do
+      read line
+      message_arr+=($line)
+      if [[ "${#line}" -eq 1 ]]
+      then
+        check=false
+      if
+    done
+    method=${message_arr[0]}
+    path=${message_arr[1]}
+    if [[ $method = 'GET' ]]
+    then
+      dir='./www/'
+      if [[ -f "$dir$path" ]]
+      then
+        printf "HTTP/1.1 200 OK\r\n"
+        printf "Content-Type: text/html; charset=utf-8\r\n\r\n"
+        cat "$dir$path"
+        printf "\r\n"
+      else
+        printf "HTTP/1.1 404 Not Found\r\n"
+        printf "Content-Length: 0\r\n\r\n"
+      fi
+    else
+      printf "HTTP/1.1 400 Bad Request\r\n"
+      printf "Content-Length: 0\r\n\r\n"
+    fi
   done
 }
 
 coproc SERVER_PROCESS { server; }
 
 netcat -lv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
+
